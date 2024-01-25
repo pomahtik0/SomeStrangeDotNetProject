303735bb7d0ad46e16de473325c3158237c10189
Roman Havriluk <pomahtik_inc@outlook.com>
2024-01-25 14:51:56 +0200

    tree component has a key value

--- Models/JSON translate model/TreeComponent.cs
+++ Models/JSON translate model/TreeComponent.cs
@@ -3,6 +3,7 @@
     public abstract class TreeComponent
     {
         public int Id { get; set; }
+        public string? Key {  get; set; }
         public abstract void PrintComponent();
     }
 }

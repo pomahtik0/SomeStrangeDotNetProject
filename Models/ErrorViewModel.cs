160f7ac949d8ec50edff51e43b9296af3112d2bb
Roman Havriluk <pomahtik_inc@outlook.com>
2024-01-25 14:20:28 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SomeStrangeDotNetProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

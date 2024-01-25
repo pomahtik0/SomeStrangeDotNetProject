1a1d994c9086140be7cb4797af1ab5f785c573a8
Roman Havriluk <pomahtik_inc@outlook.com>
2024-01-25 23:13:35 +0200

    reoverriding

--- Models/JSON translate model/CollectionDataTypes/TreeObject.cs
+++ Models/JSON translate model/CollectionDataTypes/TreeObject.cs
@@ -1,9 +1,11 @@
-﻿namespace SomeStrangeDotNetProject.Models.JSON_translate_model.CollectionDataTypes
+﻿using System.Text.Json;
+
+namespace SomeStrangeDotNetProject.Models.JSON_translate_model.CollectionDataTypes
 {
     public class TreeObject : TreeComponent
     {
         protected List<TreeComponent> children = [];
-        public override void PrintComponent()
+        public override void ReadFromJson(JsonElement jsonElement)
         {
             throw new NotImplementedException();
         }

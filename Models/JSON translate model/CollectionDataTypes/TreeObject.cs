023501f70f91b698460fe29b4af2fa9af20e3231
Roman Havriluk <pomahtik_inc@outlook.com>
2024-01-25 14:40:50 +0200

    implimenting tree component class

--- Models/JSON translate model/CollectionDataTypes/TreeObject.cs
+++ Models/JSON translate model/CollectionDataTypes/TreeObject.cs
@@ -1,6 +1,10 @@
 ﻿namespace SomeStrangeDotNetProject.Models.JSON_translate_model.CollectionDataTypes
 {
-    public class TreeObject
+    public class TreeObject : TreeComponent
     {
+        public override void PrintComponent()
+        {
+            throw new NotImplementedException();
+        }
     }
 }

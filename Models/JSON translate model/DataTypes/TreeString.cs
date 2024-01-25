f87a5a447e0867f735f651ac4d9a0a9026b66f46
Roman Havriluk <pomahtik_inc@outlook.com>
2024-01-25 23:21:51 +0200

    reoverriding

--- Models/JSON translate model/DataTypes/TreeString.cs
+++ Models/JSON translate model/DataTypes/TreeString.cs
@@ -1,8 +1,10 @@
-﻿namespace SomeStrangeDotNetProject.Models.JSON_translate_model.DataTypes
+﻿using System.Text.Json;
+
+namespace SomeStrangeDotNetProject.Models.JSON_translate_model.DataTypes
 {
     public class TreeString : TreeComponent
     {
-        public override void ReadFromJson()
+        public override void ReadFromJson(JsonElement jsonElement)
         {
             throw new NotImplementedException();
         }

ccb70f2f4b556aea03d8d4da347a7007cd28ca56
Roman Havriluk <pomahtik_inc@outlook.com>
2024-01-25 14:26:00 +0200

    basic interface created

---
+++ Models/JSON translate model/IComponent.cs
@@ -0,0 +1,8 @@
+﻿namespace SomeStrangeDotNetProject.Models.JSON_translate_model
+{
+    public interface IComponent
+    {
+        public int Id { get; set; }
+        public abstract void PrintComponent();
+    }
+}

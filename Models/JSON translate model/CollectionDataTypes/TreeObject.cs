44e903243609e6b4c0f0a4c13077596414303a02
Roman Havriluk <pomahtik_inc@outlook.com>
2024-01-26 17:24:43 +0200

    more renamings

--- Models/JSON translate model/CollectionDataTypes/TreeObject.cs
+++ Models/JSON translate model/CollectionDataTypes/TreeObject.cs
@@ -66,19 +66,19 @@ namespace SomeStrangeDotNetProject.Models.JSON_translate_model.CollectionDataTyp
             throw new NotImplementedException();
         }
 
-        public override int DbSave(SqlConnection connection, int root_id)
+        public override int DbSave(SqlConnection connection, int tree_id)
         {
-            using (var command = new SqlCommand(@"INSERT INTO Objects([Key], [Separator], [Parent_id], [Root_id]) OUTPUT inserted.Id VALUES(@Key, @Separator, @Parent_id, @Root_id)", connection)) // saving object
+            using (var command = new SqlCommand(@"INSERT INTO Objects([Key], [Separator], [Parent_id], [Tree_id]) OUTPUT inserted.Id VALUES(@Key, @Separator, @Parent_id, @Tree_id)", connection)) // saving object
             {
                 command.Parameters.AddWithValue("@Key", Key ?? (object)DBNull.Value);
                 command.Parameters.AddWithValue("@Separator", "Object");
                 command.Parameters.AddWithValue("@Parent_id", Parent?.Id ?? (object)DBNull.Value);
-                command.Parameters.AddWithValue("@Root_id", root_id);
+                command.Parameters.AddWithValue("@Tree_id", tree_id);
                 connection.Open();
                 this.Id = Convert.ToInt32(command.ExecuteScalar());
                 connection.Close();
             }
-            children.ForEach(child => child.DbSave(connection, root_id)); // saving children of the object
+            children.ForEach(child => child.DbSave(connection, tree_id)); // saving children of the object
             return Id;
         }
 

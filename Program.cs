a5bcdc257ad61b65d807b7177f266c35c27605c4
Roman Havriluk <pomahtik_inc@outlook.com>
2024-01-26 19:22:45 +0200

    building session

--- Program.cs
+++ Program.cs
@@ -9,6 +9,15 @@ namespace SomeStrangeDotNetProject
             // Add services to the container.
             builder.Services.AddControllersWithViews();
 
+            builder.Services.AddDistributedMemoryCache();
+
+            builder.Services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromSeconds(10);
+                options.Cookie.HttpOnly = true;
+                options.Cookie.IsEssential = true;
+            });
+
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
@@ -21,6 +30,8 @@ namespace SomeStrangeDotNetProject
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseSession();
 
             app.MapControllerRoute(
                 name: "default",

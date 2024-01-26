4885adfcf371a0d299b734b546c39eac9500ffa8
Roman Havriluk <pomahtik_inc@outlook.com>
2024-01-26 14:13:09 +0200

    saving object to db as string

--- Models/JSON translate model/DataTypes/TreeString.cs
+++ Models/JSON translate model/DataTypes/TreeString.cs
@@ -14,7 +14,18 @@ namespace SomeStrangeDotNetProject.Models.JSON_translate_model.DataTypes
 
         public override int DbSave(SqlConnection connection, int root_id)
         {
-            throw new NotImplementedException();
+            using (var command = new SqlCommand(@"INSERT INTO Objects([Key], [Value], [Separator], [Parent_id], [Root_id]) OUTPUT inserted.Id VALUES(@Key, @Value, @Separator, @Parent_id, @Root_id)", connection))
+            {
+                command.Parameters.AddWithValue("@Key", Key ?? (object)DBNull.Value);
+                command.Parameters.AddWithValue("@Value", Value ?? (object)DBNull.Value);
+                command.Parameters.AddWithValue("@Separator", "String");
+                command.Parameters.AddWithValue("@Parent_id", Parent?.Id ?? (object)DBNull.Value);
+                command.Parameters.AddWithValue("@Root_id", root_id);
+                connection.Open();
+                this.Id = Convert.ToInt32(command.ExecuteScalar());
+                connection.Close();
+            }
+            return Id;
         }
 
         public override void ReadFromJson(JsonElement jsonElement)

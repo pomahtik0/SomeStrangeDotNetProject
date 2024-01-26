f7ac93afb9f21aaef463d4c143ba15944b66293c
Roman Havriluk <pomahtik_inc@outlook.com>
2024-01-26 17:26:28 +0200

    renamings for readable code

--- Models/JSON translate model/DataTypes/TreeString.cs
+++ Models/JSON translate model/DataTypes/TreeString.cs
@@ -13,15 +13,15 @@ namespace SomeStrangeDotNetProject.Models.JSON_translate_model.DataTypes
             throw new NotImplementedException();
         }
 
-        public override int DbSave(SqlConnection connection, int root_id)
+        public override int DbSave(SqlConnection connection, int tree_id)
         {
-            using (var command = new SqlCommand(@"INSERT INTO Objects([Key], [Value], [Separator], [Parent_id], [Root_id]) OUTPUT inserted.Id VALUES(@Key, @Value, @Separator, @Parent_id, @Root_id)", connection)) // saving object
+            using (var command = new SqlCommand(@"INSERT INTO Objects([Key], [Value], [Separator], [Parent_id], [Tree_id]) OUTPUT inserted.Id VALUES(@Key, @Value, @Separator, @Parent_id, @Tree_id)", connection)) // saving object
             {
                 command.Parameters.AddWithValue("@Key", Key ?? (object)DBNull.Value);
                 command.Parameters.AddWithValue("@Value", Value ?? (object)DBNull.Value);
                 command.Parameters.AddWithValue("@Separator", "String");
                 command.Parameters.AddWithValue("@Parent_id", Parent?.Id ?? (object)DBNull.Value);
-                command.Parameters.AddWithValue("@Root_id", root_id);
+                command.Parameters.AddWithValue("@Tree_id", tree_id);
                 connection.Open();
                 this.Id = Convert.ToInt32(command.ExecuteScalar());
                 connection.Close();

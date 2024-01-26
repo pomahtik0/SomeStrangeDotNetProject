3aca44881a6db9b7457746893b907682761c4c33
Roman Havriluk <pomahtik_inc@outlook.com>
2024-01-26 14:11:53 +0200

    implimenting db save/read methods

--- Models/JSON translate model/DataTypes/TreeString.cs
+++ Models/JSON translate model/DataTypes/TreeString.cs
@@ -1,10 +1,22 @@
-﻿using System.Text.Json;
+﻿using System.Data.SqlClient;
+using System.Text.Json;
 
 namespace SomeStrangeDotNetProject.Models.JSON_translate_model.DataTypes
 {
     public class TreeString : TreeComponent
     {
         public string Value { get; set; } = "";
+
+        public override int DbRead(SqlConnection connection, int root_id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int DbSave(SqlConnection connection, int root_id)
+        {
+            throw new NotImplementedException();
+        }
+
         public override void ReadFromJson(JsonElement jsonElement)
         {
             Value = jsonElement.GetString() ?? "";

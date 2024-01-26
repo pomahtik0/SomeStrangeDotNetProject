1c8ce2573c8c2fcdca2cc1818395e43cc0762a2a
Roman Havriluk <pomahtik_inc@outlook.com>
2024-01-26 13:51:18 +0200

    value should always exist

--- Models/JSON translate model/DataTypes/TreeString.cs
+++ Models/JSON translate model/DataTypes/TreeString.cs
@@ -4,10 +4,10 @@ namespace SomeStrangeDotNetProject.Models.JSON_translate_model.DataTypes
 {
     public class TreeString : TreeComponent
     {
-        public string? Value { get; set; }
+        public string Value { get; set; } = "";
         public override void ReadFromJson(JsonElement jsonElement)
         {
-            Value = jsonElement.GetString();
+            Value = jsonElement.GetString() ?? "";
         }
     }
 }

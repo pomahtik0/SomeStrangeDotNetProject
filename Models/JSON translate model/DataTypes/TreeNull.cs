40222dc5e275f34d5bf9e1e7956a50d6ada60af3
Roman Havriluk <pomahtik_inc@outlook.com>
2024-01-27 15:00:01 +0200

    treeNull

---
+++ Models/JSON translate model/DataTypes/TreeNull.cs
@@ -0,0 +1,24 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using System.Text.Json;
+
+namespace SomeStrangeDotNetProject.Models.JSON_translate_model.DataTypes
+{
+    public class TreeNull : TreeComponent
+    {
+        public override void DbRead(DataTable table)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int DbSave(SqlConnection connection, int tree_id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void ReadFromJson(JsonElement jsonElement)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

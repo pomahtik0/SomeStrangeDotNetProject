a56f85a25e4adf468698f0c53cebea892ba39009
Roman Havriluk <pomahtik_inc@outlook.com>
2024-01-27 14:32:15 +0200

    creating tables if they do not exist (i hope)

--- Models/ValidatingDb.cs
+++ Models/ValidatingDb.cs
@@ -53,11 +53,36 @@ namespace SomeStrangeDotNetProject.Models
         private void CreateTablesIfNotExist(SqlConnection conn)
         {
             if (DataTablesExist(conn)) return;
+            conn.Open();
+
+            string sql = "DROP TABLE IF EXISTS table1, table2";
+            using (SqlCommand cmd = new SqlCommand(sql, conn))
+            {
+                cmd.ExecuteNonQuery();
+            }
+
+            sql = "CREATE TABLE [dbo].[Objects] ( [Id] INT IDENTITY (1, 1) NOT NULL, [Key] VARCHAR (120) NULL, [Value] VARCHAR (255) NULL, [Separator] VARCHAR (7) NOT NULL, [Parent_id] INT NULL, [Tree_id] INT NOT NULL, PRIMARY KEY CLUSTERED ([Id] ASC), CONSTRAINT [Selfrefference_FK] FOREIGN KEY ([Parent_id]) REFERENCES [dbo].[Objects] ([Id]) )";
+            using (SqlCommand cmd = new SqlCommand(sql, conn))
+            {
+                cmd.ExecuteNonQuery();
+            }
+
+            sql = "CREATE TABLE [dbo].[Trees] ([Id] INT IDENTITY (1, 1) NOT NULL, [Name] VARCHAR (100) NOT NULL, [Root_id] INT NULL, PRIMARY KEY CLUSTERED ([Id] ASC), CONSTRAINT [FK_Trees_ToObjects] FOREIGN KEY ([Root_id]) REFERENCES [dbo].[Objects] ([Id]) )";
+            using (SqlCommand cmd = new SqlCommand(sql, conn))
+            {
+                cmd.ExecuteNonQuery();
+            }
+
+            sql = "ALTER TABLE [dbo].[Objects] ADD CONSTRAINT [FK_ToRootTable] FOREIGN KEY ([Tree_id]) REFERENCES [dbo].[Trees] ([Id]) ON DELETE CASCADE";
+            using (SqlCommand cmd = new SqlCommand(sql, conn))
+            {
+                cmd.ExecuteNonQuery();
+            }
         }
 
         private bool TablesSignatureFits(SqlConnection conn)
         {
-
+            return true; // have no idea yet
         }
     }
 }

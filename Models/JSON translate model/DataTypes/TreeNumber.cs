baa049d6a91b4f4002c58b0c623c1a04f2bd88c8
Roman Havriluk <pomahtik_inc@outlook.com>
2024-01-27 15:11:56 +0200

    saving number to db

--- Models/JSON translate model/DataTypes/TreeNumber.cs
+++ Models/JSON translate model/DataTypes/TreeNumber.cs
@@ -16,7 +16,18 @@ namespace SomeStrangeDotNetProject.Models.JSON_translate_model.DataTypes
 
         public override int DbSave(SqlConnection connection, int tree_id)
         {
-            throw new NotImplementedException();
+            using (var command = new SqlCommand(@"INSERT INTO Objects([Key], [Value], [Separator], [Parent_id], [Tree_id]) OUTPUT inserted.Id VALUES(@Key, @Value, @Separator, @Parent_id, @Tree_id)", connection)) // saving object
+            {
+                command.Parameters.AddWithValue("@Key", Key ?? (object)DBNull.Value);
+                command.Parameters.AddWithValue("@Value", Value.ToString());
+                command.Parameters.AddWithValue("@Separator", "Number");
+                command.Parameters.AddWithValue("@Parent_id", Parent?.Id ?? (object)DBNull.Value);
+                command.Parameters.AddWithValue("@Tree_id", tree_id);
+                connection.Open();
+                this.Id = Convert.ToInt32(command.ExecuteScalar());
+                connection.Close();
+            }
+            return Id;
         }
 
         public override void ReadFromJson(JsonElement jsonElement)

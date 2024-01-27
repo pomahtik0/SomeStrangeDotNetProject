d323c6e1b99fc87dc5343e4a060de6543b1ccbc1
Roman Havriluk <pomahtik_inc@outlook.com>
2024-01-27 15:15:51 +0200

    reading bool from db

--- Models/JSON translate model/DataTypes/TreeBool.cs
+++ Models/JSON translate model/DataTypes/TreeBool.cs
@@ -7,9 +7,11 @@ namespace SomeStrangeDotNetProject.Models.JSON_translate_model.DataTypes
     public class TreeBool : TreeComponent
     {
         public bool Value { get; set; }
-        public override void DbRead(DataTable table)
+
+        public override void DbRead(DataTable dataTable)
         {
-            throw new NotImplementedException();
+            var value = dataTable.AsEnumerable().Where(row => row.Field<int>("Id").Equals(Id)).Select(row => row.Field<string>("Value")).FirstOrDefault();
+            this.Value = bool.Parse(value ?? "False");
         }
 
         public override int DbSave(SqlConnection connection, int tree_id)

6c6e54553ec83e0727e8db4d48515a79d0970e18
Roman Havriluk <pomahtik_inc@outlook.com>
2024-01-27 09:44:59 +0200

    model of tree created

---
+++ Models/JSON translate model/TreeModel.cs
@@ -0,0 +1,11 @@
+﻿using SomeStrangeDotNetProject.Models.JSON_translate_model.CollectionDataTypes;
+
+namespace SomeStrangeDotNetProject.Models.JSON_translate_model
+{
+    public class TreeModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = "";
+        public TreeObject? TreeRoot { get; set; }
+    }
+}

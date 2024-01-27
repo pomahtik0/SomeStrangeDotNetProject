8d6950234903ffbd5f01e07199a223c140d1ed93
Roman Havriluk <pomahtik_inc@outlook.com>
2024-01-27 15:03:51 +0200

    saving and reading null to db

--- Models/JSON translate model/DataTypes/TreeNull.cs
+++ Models/JSON translate model/DataTypes/TreeNull.cs
@@ -8,12 +8,23 @@ namespace SomeStrangeDotNetProject.Models.JSON_translate_model.DataTypes
     {
         public override void DbRead(DataTable table)
         {
-            throw new NotImplementedException();
+            return;
         }
 
         public override int DbSave(SqlConnection connection, int tree_id)
         {
-            throw new NotImplementedException();
+            using (var command = new SqlCommand(@"INSERT INTO Objects([Key], [Value], [Separator], [Parent_id], [Tree_id]) OUTPUT inserted.Id VALUES(@Key, @Value, @Separator, @Parent_id, @Tree_id)", connection)) // saving object
+            {
+                command.Parameters.AddWithValue("@Key", Key ?? (object)DBNull.Value);
+                command.Parameters.AddWithValue("@Value", DBNull.Value);
+                command.Parameters.AddWithValue("@Separator", "Null");
+                command.Parameters.AddWithValue("@Parent_id", Parent?.Id ?? (object)DBNull.Value);
+                command.Parameters.AddWithValue("@Tree_id", tree_id);
+                connection.Open();
+                this.Id = Convert.ToInt32(command.ExecuteScalar());
+                connection.Close();
+            }
+            return Id;
         }
 
         public override void ReadFromJson(JsonElement jsonElement)

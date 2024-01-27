6d295f98ef129986e76c6a083876b4fcec530904
Roman Havriluk <pomahtik_inc@outlook.com>
2024-01-27 08:58:16 +0200

    show trees view is now in new folder

--- Controllers/ShowTreesController.cs
+++ Controllers/ShowTreesController.cs
@@ -6,7 +6,7 @@ namespace SomeStrangeDotNetProject.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            return View("ShowTrees");
         }
     }
 }

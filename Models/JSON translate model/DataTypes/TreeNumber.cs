930d27720df9ca4e876ea7eb929db2b9b5f79e41
Roman Havriluk <pomahtik_inc@outlook.com>
2024-01-27 14:59:10 +0200

    treenumber

---
+++ Models/JSON translate model/DataTypes/TreeNumber.cs
@@ -0,0 +1,26 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using System.Text.Json;
+
+namespace SomeStrangeDotNetProject.Models.JSON_translate_model.DataTypes
+{
+    public class TreeNumber : TreeComponent
+    {
+        public double Value { get; set; }
+
+        public override void DbRead(DataTable table)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int DbSave(SqlConnection connection, int tree_id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void ReadFromJson(JsonElement jsonElement)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

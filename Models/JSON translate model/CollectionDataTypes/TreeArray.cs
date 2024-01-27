6500ccd1b66fbf9590529077c55bb74d9db42f39
Roman Havriluk <pomahtik_inc@outlook.com>
2024-01-27 15:34:40 +0200

    overriding DbSave for an array

--- Models/JSON translate model/CollectionDataTypes/TreeArray.cs
+++ Models/JSON translate model/CollectionDataTypes/TreeArray.cs
@@ -1,6 +1,23 @@
-﻿namespace SomeStrangeDotNetProject.Models.JSON_translate_model.CollectionDataTypes
+﻿using System.Data.SqlClient;
+
+namespace SomeStrangeDotNetProject.Models.JSON_translate_model.CollectionDataTypes
 {
     public class TreeArray : TreeObject
     {
+        public override int DbSave(SqlConnection connection, int tree_id)
+        {
+            using (var command = new SqlCommand(@"INSERT INTO Objects([Key], [Separator], [Parent_id], [Tree_id]) OUTPUT inserted.Id VALUES(@Key, @Separator, @Parent_id, @Tree_id)", connection)) // saving object
+            {
+                command.Parameters.AddWithValue("@Key", Key ?? (object)DBNull.Value);
+                command.Parameters.AddWithValue("@Separator", "Array");
+                command.Parameters.AddWithValue("@Parent_id", Parent?.Id ?? (object)DBNull.Value);
+                command.Parameters.AddWithValue("@Tree_id", tree_id);
+                connection.Open();
+                this.Id = Convert.ToInt32(command.ExecuteScalar());
+                connection.Close();
+            }
+            children.ForEach(child => child.DbSave(connection, tree_id)); // saving children of the object
+            return Id;
+        }
     }
 }

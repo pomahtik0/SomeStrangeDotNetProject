8d5750c4c90c31773bdf7c634e25d9cd92c6bd98
Roman Havriluk <pomahtik_inc@outlook.com>
2024-01-27 13:56:53 +0200

    validating methods

--- Models/ValidatingDb.cs
+++ Models/ValidatingDb.cs
@@ -3,5 +3,30 @@
     public class ValidatingDb(string connctionString)
     {
         private readonly string connctionString = connctionString;
+
+        public bool Validate()
+        {
+            return true;
+        }
+
+        private bool DatabaseExist()
+        {
+
+        }
+
+        private bool DataTablesExist()
+        {
+
+        }
+
+        private void CreateTablesIfNotExist()
+        {
+
+        }
+
+        private bool TablesSignatureFits()
+        {
+
+        }
     }
 }

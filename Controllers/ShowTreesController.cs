17ee339208d661d5032301fad4bb93deb0787531
Roman Havriluk <pomahtik_inc@outlook.com>
2024-01-27 08:47:39 +0200

    new controller for showtrees page

---
+++ Controllers/ShowTreesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SomeStrangeDotNetProject.Controllers
+{
+    public class ShowTreesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

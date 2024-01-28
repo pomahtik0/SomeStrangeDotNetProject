5ab375a6e487590b05b6488454c90105e62c494e
Roman Havriluk <pomahtik_inc@outlook.com>
2024-01-28 12:35:38 +0200

    custom handler to 404 error for bonus 2

--- Program.cs
+++ Program.cs
@@ -25,6 +25,16 @@ namespace SomeStrangeDotNetProject
             {
                 app.UseExceptionHandler("/Home/Error");
             }
+            app.Use(async (context, next) =>
+            {
+                await next();
+                if (context.Response.StatusCode == 404)
+                {
+                    var str = context.Request.Path.Value?.Split('/').Skip(1); 
+                    context.Request.Path = "/ShowTrees/GetTree" + $"/{string.Join('/', str)}";
+                    await next();
+                }
+            });
             app.UseStaticFiles();
 
             app.UseRouting();
